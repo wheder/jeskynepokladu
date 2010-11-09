1375d001566924a9c7960b6f2cb2fa36f30e41ef
Wheder <Wheder@9f347464-1ec1-7415-a2c0-efe841d4a252>
2010-11-09 20:35:13 +0000

    komit, korektni

--- trunk/nerek_mpi.cpp
+++ trunk/nerek_mpi.cpp
@@ -17,42 +17,85 @@
 //#define __TISKNI_FINALNI_MATICE 1
 
 #define LOOP_SIZE 200
-#define PRECISION 100000
+#define PRECISION 100
+
 
 using namespace std;
 
 double * C = NULL;
 double * O = NULL;
-int * cena = NULL;
-int * objem = NULL;
-int total_objem =0;
-int pocet = 0;
+unsigned int * objem = NULL;
+unsigned int total_objem =0;
+unsigned int pocet = 0;
 double max_objem = 0;
-int * max_arr = NULL;
 double max_sum = 0;
-int max_cnt = 0;
 
-int ** solution_matrix = NULL;
-//int ** keeeeeep_matrix = NULL;
-boost::dynamic_bitset<>** bitset_matrix = NULL;
 
 int mpi_count = 0;
 int mpi_nr = 0;
 
-struct sol_node
-  {
-     float start; // kde zacina platit tento prvek
-     float sum; //suma tohoto prvku
-     sol_node *nxt;        // Pointer to next node
-  };
+struct sol_node {
+    unsigned int start; // kde zacina platit tento prvek
+    double sum; //suma tohoto prvku
+    sol_node *nxt;        // Pointer to next node
+};
 
-struct kep_node
-  {
-     float start; // kde zacina platit tento prvek
-     bool bereme; //vezmeme odsud tento prvek?
-     kep_node *nxt;        // Pointer to next node
-  };
+struct kep_node {
+    unsigned int start; // kde zacina platit tento prvek
+    bool bereme; //vezmeme odsud tento prvek?
+    kep_node *nxt;        // Pointer to next node
+};
 
+sol_node ** resseni_matice = NULL;
+kep_node ** binarni_matice = NULL;
+
+double get_value(unsigned int radka, unsigned int bag_size) {
+    double tmp_val=0;
+    sol_node * bunka = resseni_matice[radka];
+    while (bunka->nxt != NULL && bunka->nxt->start <= bag_size) {
+        bunka = bunka->nxt;
+    }
+    tmp_val = bunka->sum;
+    return tmp_val;
+}
+bool get_bere(unsigned int radka, unsigned int bag_size) {
+    kep_node * bunka = binarni_matice[radka];
+    while (bunka->nxt != NULL && bunka->nxt->start <= bag_size) {
+        bunka = bunka->nxt;
+    }
+    return bunka->bereme;
+}
+
+void pridej_hodnotu_na_konec(unsigned int radka, unsigned int start, double hodnota) {
+    sol_node * bunka = resseni_matice[radka];
+    while (bunka->nxt != NULL) {
+        bunka = bunka->nxt;
+    }
+    sol_node * novy = new sol_node;
+
+    novy= new sol_node;
+    novy->start = start;
+    novy->sum = hodnota;
+    novy->nxt = NULL;
+
+    bunka->nxt = novy;
+}
+
+void beru(bool hodnota, unsigned int radka, unsigned int start) {
+    kep_node * bunka = binarni_matice[radka];
+    while (bunka->nxt != NULL) {
+        bunka = bunka->nxt;
+    }
+    if (bunka->bereme == hodnota) return; //nic se nemeni
+
+    kep_node * novy = new kep_node;
+    novy= new kep_node;
+    novy->start = start;
+    novy->bereme = hodnota;
+    novy->nxt = NULL;
+
+    bunka->nxt = novy;
+}
 
 
 int main(int argc, char *argv[])
@@ -86,90 +129,100 @@ int main(int argc, char *argv[])
     }
     inf>>max_objem;
     if (!inf.good()) return 5;
+    total_objem = max_objem*((unsigned int)PRECISION);
+
     inf>>pocet;
     if (!inf.good()) return 6;
     C = new double[pocet]; // cost
     O = new double[pocet]; // objem
-    cena = new int[pocet]; // cost
-    objem = new int[pocet]; // objem
+    objem = new unsigned int[pocet]; // objem
     for ( int i=0 ; i<pocet ; i++ ) { // nacteni originalniho pole
         inf>>C[i];
         if (!inf.good()) return 3;
         inf>>O[i];
         if (!inf.good()) return 4;
-        //prevedeni do INTu, at se s tim da rozumne a efektivne pracovat
-        cena[i]  = floor(C[i]* PRECISION);
-        objem[i] = floor(O[i]* PRECISION);
-
+        objem[i] = O[i]*((unsigned int)PRECISION);
     }
     inf.close();
 
-    //prevedeni do INTu, at se s tim da rozumne a efektivne pracovat
-    total_objem = floor(max_objem * PRECISION);
+
+    resseni_matice = new sol_node * [pocet+1];
+    binarni_matice = new kep_node * [pocet+1];
+
+    resseni_matice[0]= new sol_node;
+    resseni_matice[0]->start = 0;
+    resseni_matice[0]->sum = 0;
+    resseni_matice[0]->nxt = NULL;
+
+    binarni_matice[0]= new kep_node;
+    binarni_matice[0]->start = 0;
+    binarni_matice[0]->bereme = 0;
+    binarni_matice[0]->nxt = NULL;
+
+    for (int i =1; i< pocet+1; ++i) {
+        resseni_matice[i]= new sol_node;
+        resseni_matice[i]->start = 0;
+        resseni_matice[i]->sum = 0;
+        resseni_matice[i]->nxt = NULL;
+
+        binarni_matice[i]= new kep_node;
+        binarni_matice[i]->start = 0;
+        binarni_matice[i]->bereme = 0;
+        binarni_matice[i]->nxt = NULL;
+    }
 
 
     if (mpi_nr == 0) cout << "max_objem: "<< max_objem<<endl;
 
 
-    //nadeklarujeme uvodni matice
-    //snad nam nedojde pamet
-    solution_matrix = new int * [pocet+1];
-//    keeeeeep_matrix = new int * [pocet+1];
 
-    bitset_matrix = new boost::dynamic_bitset <> * [pocet+1];
 
-    for (int i =0; i< pocet+1; ++i) {
-        solution_matrix[i] = new int [total_objem+1];
-//        keeeeeep_matrix[i] = new int [total_objem+1];
-        bitset_matrix[i] = new boost::dynamic_bitset<> (total_objem+1);
-    }
-
-    #ifdef __TEST
-        //na vypis minusujeme zbytek
-        for (int radka = 1;radka<pocet+1; ++radka) {//do nuly tam vyplnime dycky rovnou
-            for(int bunka = 0; bunka <total_objem+1;++bunka) {
-                solution_matrix[radka][bunka] = -1;
-                (*bitset_matrix[radka])[bunka] = 0;
-            }
-        }
-    #endif /* __TEST */
-
-    for (int i=0;i<total_objem+1;++i) {
-        solution_matrix[0][i] = 0;
-        (*bitset_matrix[0])[i] = 0;
-    }
 
     if (mpi_nr == 0) {//hlavni rozdelovaci proces
         //postupne zacneme rozdelovat praci, dokud nebude cela rozdelena.
         //budeme postupovat trojuhelnikovite iteracne
         //takze kdyz bude vice procesu nez prvku co mame, tak si teda nezamakaji.
-
-
-
     }
     else {//workeri
-
-
-
     }
 
+
+
+
     //ted ten iteracni algoritmus http://www.youtube.com/watch?v=hugQNiYoqUA
-    for (int radka = 1;radka<pocet+1; ++radka) {//nuly uz jsme vyplnili vsude, tak zacneme od 1
-        for(int bunka = 0; bunka <total_objem+1;++bunka) {
+    for (unsigned int radka = 1;radka<pocet+1; ++radka) {//nuly uz jsme vyplnili vsude, tak zacneme od 1
+
+        for(unsigned int bunka = 0; bunka <total_objem+1;++bunka) {
+
             if (objem[(radka-1)] <= bunka) {//prvek by se mohl vejit
-                int suma_kdyz_vezmu = cena[radka-1] + solution_matrix[radka-1][bunka-objem[radka-1]];
-                if (solution_matrix[radka-1][bunka] < suma_kdyz_vezmu   ) {//je vetsi, napereme to tam
-                    (*bitset_matrix[radka])[bunka] = 1;
-                    solution_matrix[radka][bunka] = suma_kdyz_vezmu;
+                double uprow = get_value(radka-1, bunka);
+                double suma_kdyz_vezmu = get_value(radka-1, bunka-objem[radka-1]) + C[radka-1];
+
+                if (uprow < suma_kdyz_vezmu   ) {//je vetsi, napereme to tam
+                    pridej_hodnotu_na_konec(radka, bunka, suma_kdyz_vezmu);
+                    beru(true, radka, bunka);
                 }
                 else {//je nevyhodne to vzit
-                    solution_matrix[radka][bunka] = solution_matrix[radka-1][bunka];
-                    (*bitset_matrix[radka])[bunka] = 0;
+                    double predchozi = get_value(radka, bunka);
+                    if (uprow != predchozi) {
+                        pridej_hodnotu_na_konec(radka, bunka, uprow);
+                    }
+                    beru(false, radka, bunka);
                 }
+
+
+
             }
             else {//nevejde se, tak opiseme to co je nad tim
-                solution_matrix[radka][bunka] = solution_matrix[radka-1][bunka];
-                (*bitset_matrix[radka])[bunka] = 0;
+                double uprow = get_value(radka-1, bunka);
+                double predchozi = get_value(radka, bunka);
+                if (uprow != predchozi) {
+                    pridej_hodnotu_na_konec(radka, bunka, uprow);
+                }
+                //kdyz se nevejde, tak na to sereme, a jelikoz mame zinicializovano nulama, tak tam ta nula je.
+
+
+
             }
             /*
             #ifdef __TEST
@@ -203,7 +256,6 @@ int main(int argc, char *argv[])
                 }
                 printf("\n");
             #endif /* __TEST */
-
         }
     }
     //vyplnene matice
@@ -239,25 +291,24 @@ int main(int argc, char *argv[])
     printf("\n");
 #endif /* __TISKNI_FINALNI_MATICE */
 
-
-
-    int part_objem = total_objem;
-    int dosazena_cena = 0;
+    unsigned int part_objem = total_objem;
+    cout <<"sss" <<part_objem << endl;
+    cout <<"sss" <<pocet << endl;
+    double dosazena_cena = 0;
     for (int index_prvku = pocet; index_prvku > 0; --index_prvku) {
-        if ((*bitset_matrix[index_prvku])[part_objem] == 1 ) {
-            printf("%d | %.12f | %.12f \n",index_prvku, ((float)(cena[index_prvku-1])/PRECISION),((float)(objem[index_prvku-1])/PRECISION)); // floaty
-            //printf("idx %d |  cena %d | objem %d \n",index_prvku, cena[index_prvku-1],objem[index_prvku-1]); //INTY
+        if (get_bere(index_prvku, part_objem) ) {
+            printf("%d | %.12f | %.12f \n",index_prvku, C[index_prvku-1],((float)(objem[index_prvku-1])/PRECISION)); // floaty
             part_objem -= objem[index_prvku-1];
-            dosazena_cena += cena[index_prvku-1];
+//    cout <<"sss" <<part_objem << endl;
+            dosazena_cena += C[index_prvku-1];
         }
 
 
 
     }
 
-    printf("cena: %.12f",((float)dosazena_cena/PRECISION));
+    printf("cena: %.12f",dosazena_cena);
     cout  << endl;
-
 
 //system("read -p a");
 
